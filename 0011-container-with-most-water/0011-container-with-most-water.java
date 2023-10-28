6f7d7e198d7f97c8562d67b314cf24a30940d79f
Adiah <83296434+Adiah-A@users.noreply.github.com>
2023-10-28 12:31:46 -0400

    Time: 2 ms (99.63%), Space: 55.5 MB (45.69%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,28 +1,40 @@
 class Solution {
     public int maxArea(int[] height) {
-        int left= 0;
-        int right = height.length - 1;
-        int maxArea = Integer.MIN_VALUE;
+        int area = Integer.MIN_VALUE;
 
-        int area = (right - left) * Math.min(height[left], height[right]);
-        maxArea = Math.max(area, maxArea);
+        int left = 0;
+        int right = height.length - 1;
+
 
         while(left < right){
+
+            int currArea = maxWaterArea(left, right, height);
+            area = Math.max(area, currArea);
+            
+            //We need to a way to know if it is teh left or right that has the smallest width and use that to know which to advance till we find a higher width
+
             if(height[left] < height[right]){
-                left ++;
-                int newArea = (right - left) * Math.min(height[left], height[right]);
-                maxArea = Math.max(newArea, maxArea);
-            }else if(height[left] > height[right]){
-                right--;
-                int newArea = (right - left) * Math.min(height[left], height[right]);
-                maxArea = Math.max(newArea, maxArea);
+                int tempHeight = height[left];
+                while(left < right && height[left] <= tempHeight){
+                    left++;
+                }
             }else{
-                left ++;
-                int newArea = (right - left) * Math.min(height[left], height[right]);
-                maxArea = Math.max(newArea, maxArea);
+                int tempHeight = height[right];
+                while(left < right && height[right] <= tempHeight){
+                    right--;
+                }
             }
+
         }
 
-        return maxArea;
+        return area;
+    }
+
+    private int maxWaterArea(int left, int right, int[] height){
+        int length = right - left;
+        int width = Math.min(height[left], height[right]);
+        int area = length * width;
+
+        return area;
     }
 }
